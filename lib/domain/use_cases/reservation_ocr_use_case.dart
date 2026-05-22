c43181aae1517c157fbc1184cae49e8a581c7d44
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-05-23 00:19:54 +0900

    feat: #10 - ReservationOcrUseCase 및 Repository 인터페이스 추가

---
+++ lib/domain/use_cases/reservation_ocr_use_case.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:studio_chance/domain/entities/reservation_ocr_result.dart';
+import 'package:studio_chance/domain/repository_interfaces/reservation_ocr_repository.dart';
+
+abstract interface class ReservationOcrUseCase {
+  Future<Either<Exception, ReservationOcrResult>> execute(Uint8List imageBytes);
+}
+
+class ReservationOcrUseCaseImpl implements ReservationOcrUseCase {
+  final ReservationOcrRepository _repository;
+
+  ReservationOcrUseCaseImpl({required ReservationOcrRepository repository})
+      : _repository = repository;
+
+  @override
+  Future<Either<Exception, ReservationOcrResult>> execute(
+    Uint8List imageBytes,
+  ) {
+    return _repository.analyzeReservationImage(imageBytes);
+  }
+}
