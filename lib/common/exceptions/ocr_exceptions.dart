36a1c9b23f3fca67622eddb4aaa549fa6d7b802e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-05-23 00:17:33 +0900

    feat: #10 - OcrException isSilentable 명시적 오버라이드 추가

--- lib/common/exceptions/ocr_exceptions.dart
+++ lib/common/exceptions/ocr_exceptions.dart
@@ -19,6 +19,13 @@ sealed class OcrException extends AppException {
     OcrUnknownException() =>
       '일시적인 에러가 발생했습니다.\n잠시 후 다시 시도해 주세요.',
   };
+
+  @override
+  bool get isSilentable => switch (this) {
+    OcrNetworkException() ||
+    OcrParsingException() ||
+    OcrUnknownException() => false,
+  };
 }
 
 class OcrNetworkException extends OcrException {
